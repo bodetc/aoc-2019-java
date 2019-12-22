ebdfdcd7a3b4b9fa3e8d2d87dc29f133e8471f8a
Cédric Bodet <cedric.bodet@futurice.com>
2019-12-22 12:31:05 +0200

    Advent of Code 2019 - Day 22 * Day 22: Create SpaceCard with shuffling

---
+++ src/main/java/net/spizzer/aoc2019/day22/SpaceCards.java
@@ -0,0 +1,59 @@
+package net.spizzer.aoc2019.day22;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SpaceCards {
+    private final int[] deck;
+    private final int length;
+
+    private SpaceCards(int[] cards) {
+        this.deck = cards;
+        this.length = cards.length;
+    }
+
+    public static SpaceCards factoryOrder(int length) {
+        return new SpaceCards(IntStream.range(0, length).toArray());
+    }
+
+    @VisibleForTesting
+    static SpaceCards fromArray(int[] cards) {
+        return new SpaceCards(Arrays.copyOf(cards, cards.length));
+    }
+
+    public SpaceCards dealIntoNewStack() {
+        int[] cards  = new int[length];
+        for(int i = 0; i<length; i++) {
+            cards[i] = deck[length-1-i];
+        }
+        return new SpaceCards(cards);
+    }
+
+    public SpaceCards cut(int N) {
+        int cut = Math.floorMod(N, length);
+        int[] cards  = new int[length];
+        System.arraycopy(deck, cut, cards, 0, length-cut);
+        System.arraycopy(deck, 0, cards, length-cut, cut);
+
+        return new SpaceCards(cards);
+    }
+
+    public SpaceCards dealWithIncrement(int N) {
+        int[] cards  = new int[length];
+        for(int i = 0; i<length; i++) {
+            cards[(N*i)%length] = deck[i];
+        }
+        return new SpaceCards(cards);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof SpaceCards && equals((SpaceCards) other);
+    }
+
+    private boolean equals(SpaceCards other) {
+        return length==other.length && Arrays.equals(deck, other.deck);
+    }
+}
---
+++ src/test/java/net/spizzer/aoc2019/day22/SpaceCardsTest.java
@@ -0,0 +1,87 @@
+package net.spizzer.aoc2019.day22;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpaceCardsTest {
+
+    @Test
+    void testDealIntoNewStack() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .dealIntoNewStack();
+
+        assertEquals(SpaceCards.fromArray(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}), spaceCards);
+    }
+
+    @Test
+    void testCut() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .cut(3);
+
+        assertEquals(SpaceCards.fromArray(new int[]{3, 4, 5, 6, 7, 8, 9, 0, 1, 2}), spaceCards);
+    }
+
+    @Test
+    void testCutNegative() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .cut(-4);
+
+        assertEquals(SpaceCards.fromArray(new int[]{6, 7, 8, 9, 0, 1, 2, 3, 4, 5}), spaceCards);
+    }
+
+    @Test
+    void testDealWithIncrement() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .dealWithIncrement(3);
+
+        assertEquals(SpaceCards.fromArray(new int[]{0, 7, 4, 1, 8, 5, 2, 9, 6, 3}), spaceCards);
+    }
+
+    @Test
+    void test1() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .dealWithIncrement(7)
+                .dealIntoNewStack()
+                .dealIntoNewStack();
+
+        assertEquals(SpaceCards.fromArray(new int[]{0, 3, 6, 9, 2, 5, 8, 1, 4, 7}), spaceCards);
+    }
+
+    @Test
+    void test2() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .cut(6)
+                .dealWithIncrement(7)
+                .dealIntoNewStack();
+
+        assertEquals(SpaceCards.fromArray(new int[]{3, 0, 7, 4, 1, 8, 5, 2, 9, 6}), spaceCards);
+    }
+
+    @Test
+    void test3() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .dealWithIncrement(7)
+                .dealWithIncrement(9)
+                .cut(-2);
+
+        assertEquals(SpaceCards.fromArray(new int[]{6, 3, 0, 7, 4, 1, 8, 5, 2, 9}), spaceCards);
+    }
+
+    @Test
+    void test4() {
+        SpaceCards spaceCards = SpaceCards.factoryOrder(10)
+                .dealIntoNewStack()
+                .cut(-2)
+                .dealWithIncrement(7)
+                .cut(8)
+                .cut(-4)
+                .dealWithIncrement(7)
+                .cut(3)
+                .dealWithIncrement(9)
+                .dealWithIncrement(3)
+                .cut(-1);
+
+        assertEquals(SpaceCards.fromArray(new int[]{9,2,5,8,1,4,7,0,3,6}), spaceCards);
+    }
+}
